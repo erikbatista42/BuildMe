cc963c76ff207a9b010acaf36740655cebdb60fc
luxury <luxury@luxury.local>
2018-11-10 11:53:25 -0800

    added and edited doc string

---
+++ BuildMe/BuildMe/Docs.string.swift
@@ -0,0 +1,49 @@
+//
+//  Docs.string.swift
+//  BuildMe
+//
+//  Created by luxury on 11/10/18.
+//  Copyright © 2018 luxury. All rights reserved.
+//
+
+
+        // [Setup]
+/*
+ - Setup project with folders and stuff
+ - Setup to make it programming only. No storyboard.
+ -
+*/
+
+
+
+        // [To Do]
+/*
+ 
+ [View One (Main Screen with Categories)]
+    -> [Design]
+         - Add Navigation Bar
+         - Set up Nav Bar Icons (settings, Logo, Add)
+         - Setup Collection View
+         - setup another
+         - When clicked on Add Icon, camera shows up so you can start recording and then edit when finished
+ 
+    -> [Firebase]
+         - Fetch uploaded videos
+         -
+ 
+ [View Two Design (Selected Category)]
+         - Setup Collection View
+         - Back button works
+ 
+     -> [Firebase]
+         - Fetch Videos based on selected category
+         - 
+*/
+
+
+
+
+        // [Problems ran into/learned]
+/*
+ -
+*/
